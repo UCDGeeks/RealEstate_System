68ea758cc1ab4886552180dc6946fa64eb4485ed
H.L.Kalana Kaushanga Chandrasiri <hlckalana@yahoo.com>
2015-03-19 00:25:47 +0530

    Main form start button code
    but i did not link main class so forms are not run

--- src/realstate_system/RealState_System.java
+++ src/realstate_system/RealState_System.java
@@ -6,17 +6,35 @@
 
 package realstate_system;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import views.main;
+
 /**
  *
  * @author Kalana Chandrasiri
  */
+
+
+
 public class RealState_System {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         // TODO code application logic here
+       
+       //main myFrame = new main ();
+       
+        SwingUtilities.invokeLater(new Runnable() {
+                       @Override
+			public void run() {
+				main main = new main();
+				//main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				main.setVisible(true);
+			}
+		});
     }
     
 }
--- src/views/main.java
+++ src/views/main.java
@@ -5,17 +5,23 @@
  */
 package views;
 
+import javax.swing.JFrame;
+import realstate_system.RealState_System;
+
 /**
  *
  * @author S.Priyanga
  */
 public class main extends javax.swing.JFrame {
-
+  
+   
     /**
      * Creates new form main
      */
     public main() {
+        setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
         initComponents();
+        
     }
 
     /**
@@ -48,6 +54,12 @@ public class main extends javax.swing.JFrame {
         jButton1.setForeground(new java.awt.Color(0, 153, 204));
         jButton1.setText("Start ...");
         jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        jButton1.setName("btnStart"); // NOI18N
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton1MouseClicked(evt);
+            }
+        });
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
@@ -95,40 +107,17 @@ public class main extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+        // TODO add your handling code here:
+        home m=new home();
+        m.setVisible(true);
+        setVisible(false);
+        
+    }//GEN-LAST:event_jButton1MouseClicked
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new main().setVisible(true);
-            }
-        });
-    }
+    
+    
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
@@ -136,4 +125,6 @@ public class main extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jTitle;
     // End of variables declaration//GEN-END:variables
+
+    
 }
