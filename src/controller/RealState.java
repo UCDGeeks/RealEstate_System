593ac933f5c0b5fc4d67f60fa60776bc3a4ee141
Srimal Priyanga <s.priyanga22@gmail.com>
2015-04-03 01:51:31 +0530

    code cleanup

--- src/controller/RealState.java
+++ src/controller/RealState.java
@@ -1,27 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package controller;
-
-import javax.swing.JFrame;
-import views.main;
-
 /**
  *
- * @author Kalana Chandrasiri
+ * @author Team D2Real
  */
-public class RealState_System {
+package controller;
+import javax.swing.JFrame;
+import views.houseAdd_v;
+import views.main;
+
+public class RealState {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        // TODO code application logic here
-         
+
+    /**
+     * load main view
+     */
+    public void loadMain() {
+        main viewMain = new main();
+        viewMain.main();
     }
     
+    };
 }
