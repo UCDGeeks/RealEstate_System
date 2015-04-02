9b191b72b4c70b7475e70051845a77491378a5b7
Anton Dilshan <antondilshanperera@gmail.com>
2015-04-03 01:32:11 +0530

    loading views functionality added to manageHouse controller.

--- src/controller/manageHouse.java
+++ src/controller/manageHouse.java
@@ -16,19 +16,24 @@ import models.houseAdd_m;
 import models.houseDetails_m;
 import views.houseDetails_v;
 
+import views.home;
 import org.json.simple.parser.ParseException;
 
 public class manageHouse {
     
-/***********************************************************
-************** House Add Section Functionality *************    
-***********************************************************/
+//************** House Add Section Functionality ************* 
     public houseAdd_m model;
     
     public void addHouse() {
         //Display the House Add View
-        houseAdd_v view = new houseAdd_v();
-        view.viewHouseadd();
+        houseAdd_v addHouseView = new houseAdd_v();
+        addHouseView.viewHouseadd();
+    }
+    
+    public void viewHome() {
+        //Display the House Manage Home View
+        home Homeview = new home();
+        Homeview.viewHome();
     }
 
     public boolean setValuesHouseAdd(int[] valueArrInt,String[] vlueArrStr) throws ParseException{
