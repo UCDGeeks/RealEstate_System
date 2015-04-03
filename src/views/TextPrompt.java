9989e19f07fdce5971601bb03c7b525ac92dbe60
Thisaru Sanduranga <thisaru.bitbucket@gmail.com>
2015-04-04 00:37:38 +0530

    code cleanup

--- src/views/TextPrompt.java
+++ src/views/TextPrompt.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package views;
 
 /**
  *
- * @author Anton
+ * @author Anton, Kalana
  */
 import java.awt.*;
 import java.awt.event.*;
